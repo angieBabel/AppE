fcaa639ca0eb0f09b89a5002238eb3ee15fd84c0
Jordi Alejandro García Núñez <jordign.mx@hotmail.com>
2016-09-09 19:40:53 -0500

    uodate

--- app/src/main/java/com/example/yoo/appeb/GastoFragment.java
+++ app/src/main/java/com/example/yoo/appeb/GastoFragment.java
@@ -2,6 +2,7 @@ package com.example.yoo.appeb;
 
 
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -23,7 +24,20 @@ public class GastoFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_gasto, container, false);
+        View view= inflater.inflate(R.layout.fragment_gasto, container, false);
+        FloatingActionButton fab = (FloatingActionButton) view.findViewById(R.id.fabGasto);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                addconcepto fragment = new addconcepto();
+                android.support.v4.app.FragmentTransaction fragmentTransaction =
+                        getFragmentManager().beginTransaction();
+                fragmentTransaction.replace(R.id.fragment_container, fragment);
+                fragmentTransaction.commit();
+                /*Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();*/
+            }
+        });
+        return view;
     }
-
 }
