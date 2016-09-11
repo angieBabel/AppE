2ca566f932fab801a653f14a44788f74dacb8965
Jordi Alejandro García Núñez <jordign.mx@hotmail.com>
2016-09-10 23:20:43 -0500

    navegacion
    se crearon los metodos para mostrar o esconder algunos componentes
    cuando: se elige entre contado o credito, cuando se elige entre rubro
    existente o nuevo rubro

--- app/src/main/java/com/example/yoo/appeb/agregargasto.java
+++ app/src/main/java/com/example/yoo/appeb/agregargasto.java
@@ -6,6 +6,11 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
 
 /**
@@ -23,7 +28,44 @@ public class agregargasto extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_agregargasto, container, false);
+        View view = inflater.inflate(R.layout.fragment_agregargasto, container, false);
+        //text = (TextView) view.findViewById(R.id.Productos);
+        super.onCreate(savedInstanceState);
+        return view;
     }
+    @Override
+    public void onActivityCreated(Bundle state) {
+        super.onActivityCreated(state);
+        LinearLayout layoutRE = (LinearLayout)getView().findViewById(R.id.RE);
+        LinearLayout layoutRN = (LinearLayout)getView().findViewById(R.id.RN);
+        RadioGroup RG = (RadioGroup)getView().findViewById(R.id.RubrosRB);
+        RG.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+              @Override
+              public void onCheckedChanged(RadioGroup group, int checkedId) {
+                  switch(checkedId){
+                      case R.id.rubroE:
+                          // do operations specific to this selection
+                          mostrarRE(getView());
+                          break;
 
+                      case R.id.rubroN:
+                          // do operations specific to this selection
+                          mostrarRN(getView());
+                          break;
+                  }
+              }
+          }
+        );
+    }
+    public void mostrarRE(View view) {
+        //((TextView) view.findViewById(R.id.textView_superior)).setText(mItem.textoEncima);
+        ((LinearLayout) view.findViewById(R.id.RE)).setVisibility(View.VISIBLE);
+        ((LinearLayout) view.findViewById(R.id.RN)).setVisibility(View.GONE);
+
+
+    }
+    public void mostrarRN(View view){
+        ((LinearLayout) view.findViewById(R.id.RN)).setVisibility(View.VISIBLE);
+        ((LinearLayout) view.findViewById(R.id.RE)).setVisibility(View.GONE);
+    }
 }
--- app/src/main/java/com/example/yoo/appeb/ddVenta.java
+++ app/src/main/java/com/example/yoo/appeb/ddVenta.java
@@ -6,6 +6,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.RadioGroup;
 
 
 /**
@@ -23,7 +25,46 @@ public class ddVenta extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_dd_venta, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_dd_venta, container, false);
+        super.onCreate(savedInstanceState);
+        return view;
+
     }
+
+    public void onActivityCreated(Bundle state) {
+        super.onActivityCreated(state);
+        LinearLayout deudor = (LinearLayout)getView().findViewById(R.id.deudor);
+        RadioGroup RG = (RadioGroup)getView().findViewById(R.id.ModopagoRB);
+        RG.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+                  @Override
+                  public void onCheckedChanged(RadioGroup group, int checkedId) {
+                      switch(checkedId){
+                          case R.id.pagoCredito:
+                              // do operations specific to this selection
+                              showDeudor(getView());
+                              break;
+
+                          case R.id.pagoContado:
+                              // do operations specific to this selection
+                              hideDeudor(getView());
+                              break;
+                      }
+                  }
+              }
+        );
+    }
+
+    public void showDeudor(View view) {
+        //((TextView) view.findViewById(R.id.textView_superior)).setText(mItem.textoEncima);
+        ((LinearLayout) view.findViewById(R.id.deudor)).setVisibility(View.VISIBLE);
+
+
+
+    }
+    public void hideDeudor(View view){
+        ((LinearLayout) view.findViewById(R.id.deudor)).setVisibility(View.GONE);
+    }
+
 
 }
